c00ddd3ea9e2bdef281eedf1b99f68bc66c4dc07
Ryan Davis <ryand@zenspider.com>
2009-06-22 14:40:43 -0800

    + tests forcifully clean out ~/.ruby_inline [git-p4: depot-paths = "//src/un/dev/": change = 5130]

--- test/test_un.rb
+++ test/test_un.rb
@@ -1,3 +1,11 @@
+dir = File.expand_path "~/.ruby_inline"
+if test ?d, dir then
+  require 'fileutils'
+  puts "nuking #{dir}"
+  # force removal, Windoze is bitching at me, something to hunt later...
+  FileUtils.rm_r dir, :force => true
+end
+
 require 'test/unit'
 require 'un'
 
